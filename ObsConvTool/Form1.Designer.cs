555300529d1d85e0a54dd4d048dc8df0cc2eaecb
HzYu <tim406she1016@gmail.com>
2019-12-08 19:45:48 +0800

    openfile

--- ObsConvTool/Form1.Designer.cs
+++ ObsConvTool/Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SdrTextBox = new System.Windows.Forms.TextBox();
+            this.OpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1004, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案ToolStripMenuItem
+            // 
+            this.檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile});
+            this.檔案ToolStripMenuItem.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.檔案ToolStripMenuItem.Name = "檔案ToolStripMenuItem";
+            this.檔案ToolStripMenuItem.Size = new System.Drawing.Size(66, 29);
+            this.檔案ToolStripMenuItem.Text = "檔案";
+            // 
+            // SdrTextBox
+            // 
+            this.SdrTextBox.Font = new System.Drawing.Font("新細明體", 11F);
+            this.SdrTextBox.Location = new System.Drawing.Point(24, 135);
+            this.SdrTextBox.Multiline = true;
+            this.SdrTextBox.Name = "SdrTextBox";
+            this.SdrTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.SdrTextBox.Size = new System.Drawing.Size(660, 368);
+            this.SdrTextBox.TabIndex = 1;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(224, 26);
+            this.OpenFile.Text = "開啟檔案";
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(808, 455);
+            this.ClientSize = new System.Drawing.Size(1004, 680);
+            this.Controls.Add(this.SdrTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "ObsConvTool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案ToolStripMenuItem;
+        private System.Windows.Forms.TextBox SdrTextBox;
+        private System.Windows.Forms.ToolStripMenuItem OpenFile;
     }
 }
 
--- ObsConvTool/Form1.cs
+++ ObsConvTool/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,27 @@ namespace ObsConvTool
 {
     public partial class Form1 : Form
     {
+
+        private string SdrText { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFile = new OpenFileDialog();
+            OpenFile.InitialDirectory = ".\\";
+            OpenFile.Filter = "sdr files|*.sdr|mac files|*.mac";
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader StrReader = new StreamReader(OpenFile.FileName, Encoding.Default);
+                SdrText = StrReader.ReadToEnd();
+                SdrText = SdrText.Replace(" ", "");
+                SdrTextBox.Text = SdrText;
+                StrReader.Close();
+            }
         }
     }
 }
